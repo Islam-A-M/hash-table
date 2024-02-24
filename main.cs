9c3f281680738a81c8dc772d64b5438f2a15c57e
Islam Ahmed <rabshet9@gmail.com>
2024-02-24 23:05:36 +0200

    first commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+using System;
+using islam;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+      HashTable<string, string> table = new HashTable<string, string>();
+      table.Print();
+      table.Set("Sinar", "sinar@gmail.com");
+      table.Set("Elvis", "elvis@gmail.com");
+      table.Set("Tane", "tane@gmail.com");
+      table.Print();
+      Console.WriteLine("[get] " + table.Get("Sinar"));
+    //Console.WriteLine("[get] " + table.Get("Tane"));
+      table.Set("Gerti", "gerti@gmail.com");
+      table.Set("Arist", "arist@gmail.com");
+
+      table.Print();
+      table.Remove("Arist");
+      
+      table.Print();
+      table.Set("Arist", "arist@gmail.com");
+      table.Print();
+
+      Console.WriteLine("[get] " + table.Get("Sinar"));
+  
+    }
+}
